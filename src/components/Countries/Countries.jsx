6e130eae7f10b562e6426f7058d040aa107fdd9f
Humaeid Bin Mansur <humaeidmansur@gmail.com>
2025-10-05 15:33:45 +0200

    first message

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>IN the countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
